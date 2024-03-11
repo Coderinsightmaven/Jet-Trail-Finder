f9880c8dda3077e8cc7275f191565137298ca515
Coderinsightmaven <lmarincik@proton.me>
2024-03-11 17:42:50 -0600

    fixed the slideshow video not showing up when the application is built.

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,10 +1,10 @@
-import SlideShowVideo from "../../public/SlideShow.mp4"
+
 
 export default function FullScreenVideo() {
     return (
         <div className="relative w-full h-screen overflow-hidden">
         <video
-          src={SlideShowVideo}
+          src="/SlideShow.mp4"
           autoPlay
           muted
           loop
