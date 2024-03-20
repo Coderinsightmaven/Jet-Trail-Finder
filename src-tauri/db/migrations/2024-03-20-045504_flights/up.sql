2a68ff81ce8bde14fada3bb86713bccbc4e2ecc4
Coderinsightmaven <lmarincik@proton.me>
2024-03-20 12:34:25 -0600

    edited schema to allow max_length 4 chractes for departure and arrival airport code

--- src-tauri/db/migrations/2024-03-20-045504_flights/up.sql
+++ src-tauri/db/migrations/2024-03-20-045504_flights/up.sql
@@ -2,8 +2,8 @@
 CREATE TABLE flights (
     id SERIAL PRIMARY KEY,
     flight_number VARCHAR(10) NOT NULL,
-    departure_airport_code VARCHAR(3) NOT NULL,
-    arrival_airport_code VARCHAR(3) NOT NULL,
+    departure_airport_code VARCHAR(4) NOT NULL,
+    arrival_airport_code VARCHAR(4) NOT NULL,
     departure_time TIMESTAMP NOT NULL,
     user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (user_id) -- Adjusted to reference user_id
--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -1,6 +1,20 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    flights (id) {
+        id -> Int4,
+        #[max_length = 10]
+        flight_number -> Varchar,
+        #[max_length = 4]
+        departure_airport_code -> Varchar,
+        #[max_length = 4]
+        arrival_airport_code -> Varchar,
+        departure_time -> Timestamp,
+        user_id -> Int4,
+    }
+}
+
+diesel::table! {
     users (user_id) {
         user_id -> Int4,
         #[max_length = 255]
@@ -15,3 +29,10 @@ diesel::table! {
         username -> Varchar,
     }
 }
+
+diesel::joinable!(flights -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    flights,
+    users,
+);
