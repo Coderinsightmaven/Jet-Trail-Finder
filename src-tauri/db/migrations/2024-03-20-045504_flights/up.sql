69453fa99a39d45376945e141edfdec7b0516e68
Coderinsightmaven <lmarincik@proton.me>
2024-03-20 12:29:54 -0600

    added flights to database sql

--- src-tauri/db/migrations/2024-03-20-045504_flights/up.sql
+++ src-tauri/db/migrations/2024-03-20-045504_flights/up.sql
@@ -1,1 +1,10 @@
 -- Your SQL goes here
+CREATE TABLE flights (
+    id SERIAL PRIMARY KEY,
+    flight_number VARCHAR(10) NOT NULL,
+    departure_airport_code VARCHAR(3) NOT NULL,
+    arrival_airport_code VARCHAR(3) NOT NULL,
+    departure_time TIMESTAMP NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) -- Adjusted to reference user_id
+);
